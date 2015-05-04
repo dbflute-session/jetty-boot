e842cedc0649ad4395e35b87551f41d679f6bac9
jflute <dbflute@gmail.com>
2015-05-05 00:01:11 +0900

    rename package to org.dbflute

--- src/main/java/org/dbflute/jetty/JettyBoot.java
+++ src/main/java/org/dbflute/jetty/JettyBoot.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.lastaflute.jetty;
+package org.dbflute.jetty;
 
 import java.io.File;
 import java.io.IOException;

3dd80c58278afd918971036d61ade15afe2f489f
jflute <dbflute@gmail.com>
2017-10-20 23:10:14 +0900

    can use web fragments and meta-info resources in classpath

--- src/main/java/org/dbflute/jetty/JettyBoot.java
+++ src/main/java/org/dbflute/jetty/JettyBoot.java
@@ -18,13 +18,18 @@ package org.dbflute.jetty;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,6 +37,8 @@ import org.dbflute.jetty.util.BoJtResourceUtil;
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.JarResource;
+import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.FragmentConfiguration;
 import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
@@ -72,6 +79,7 @@ public class JettyBoot {
     protected boolean useMetaInfoResourceDetect;
     protected boolean useTldDetect;
     protected boolean useWebFragmentsDetect;
+    protected Predicate<String> webFragmentsSelector;
 
     // -----------------------------------------------------
     //                                              Stateful
@@ -81,21 +89,49 @@ public class JettyBoot {
     // ===================================================================================
     //                                                                         Constructor
     //                                                                         ===========
+    /**
+     * Create with port number and context path.
+     * <pre>
+     * e.g. has context path
+     *  JettyBoot boot = new JettyBoot(8151, "/fortress");
+     * 
+     * e.g. no context path
+     *  JettyBoot boot = new JettyBoot(8151, "");
+     * </pre>
+     * @param port The port number for the jetty server.
+     * @param contextPath The context path for the jetty server, basically has slash prefix. (NotNull, EmptyAllowed)
+     */
     public JettyBoot(int port, String contextPath) {
         this.port = port;
         this.contextPath = contextPath;
     }
 
+    // -----------------------------------------------------
+    //                                                Option
+    //                                                ------
+    /**
+     * Does it boot the jetty server as development mode?
+     * @return this. (NotNull)
+     */
     public JettyBoot asDevelopment() {
         development = true;
         return this;
     }
 
+    /**
+     * Does it boot the jetty server as development mode?
+     * @param development Is it development mode?
+     * @return this. (NotNull)
+     */
     public JettyBoot asDevelopment(boolean development) {
         this.development = development;
         return this;
     }
 
+    /**
+     * Browse on desktop automatically after boot finished.
+     * @return this. (NotNull)
+     */
     public JettyBoot browseOnDesktop() {
         // wants to use this in production (e.g. DBFlute Intro) 
         //assertDevelopmentState();
@@ -103,6 +139,10 @@ public class JettyBoot {
         return this;
     }
 
+    /**
+     * Suppress shutdown hook. (for development only)
+     * @return this. (NotNull)
+     */
     public JettyBoot suppressShutdownHook() {
         assertDevelopmentState();
         suppressShutdownHook = true;
@@ -115,28 +155,65 @@ public class JettyBoot {
         }
     }
 
+    /**
+     * You can detect embedded webroot.
+     * @return this. (NotNull)
+     */
     public JettyBoot useEmbeddedWebroot() {
         useEmbeddedWebroot = true;
         return this;
     }
 
+    /**
+     * You can detect annotations in all jar files.
+     * @return this. (NotNull)
+     */
     public JettyBoot useAnnotationDetect() {
         useAnnotationDetect = true;
         return this;
     }
 
+    /**
+     * You can detect 'META-INF' resources in jar files detected as web fragments. <br>
+     * <span style="color: #CC4747; font-size: 120%">So you also needs to enable web fragments detect.</span>
+     * @return this. (NotNull)
+     */
     public JettyBoot useMetaInfoResourceDetect() {
         useMetaInfoResourceDetect = true;
         return this;
     }
 
+    /**
+     * You can detect '.tdl' files in all jar files.
+     * @return this. (NotNull)
+     */
     public JettyBoot useTldDetect() {
         useTldDetect = true;
         return this;
     }
 
+    /**
+     * You can detect web fragments in all jar files.
+     * @return this. (NotNull)
+     */
     public JettyBoot useWebFragmentsDetect() {
         useWebFragmentsDetect = true;
+        return this;
+    }
+
+    /**
+     * You can detect web fragments in selected jar files.
+     * <pre>
+     * boot.useMetaInfoResourceDetect().useWebFragmentsDetect(jarName -&gt; { // for swagger
+     *     return jarName.contains("swagger-ui");
+     * });
+     * </pre>
+     * @param oneArgLambda The callback for selector of web fragments, argument is jar name. (NotNull)
+     * @return this. (NotNull)
+     */
+    public JettyBoot useWebFragmentsDetect(Predicate<String> oneArgLambda) { // you can select
+        useWebFragmentsDetect = true;
+        webFragmentsSelector = oneArgLambda;
         return this;
     }
 
@@ -189,7 +266,7 @@ public class JettyBoot {
         try {
             path = warLocation.toURI().getPath();
         } catch (URISyntaxException e) {
-            throw new IllegalStateException("server start failed.", e);
+            throw new IllegalStateException("Failed to get the war URI: " + warLocation, e);
         }
         final WebAppContext context = new WebAppContext();
         if (path != null && isWarableFile(path)) {
@@ -199,6 +276,7 @@ public class JettyBoot {
         }
         context.setConfigurations(prepareConfigurations());
         context.setContextPath(contextPath);
+        setupClasspathJarResourceIfNeeds(context); // basically for local development (and e.g. swagger) 
         return context;
     }
 
@@ -363,11 +441,56 @@ public class JettyBoot {
     }
 
     protected boolean isValidMetaInfConfiguration() {
-        return useMetaInfoResourceDetect || useTldDetect || useWebFragmentsDetect;
+        // meta-info resource detect is triggered by web-fragment detect (to be same specification as tomcat) 
+        //return useMetaInfoResourceDetect || useTldDetect || useWebFragmentsDetect;
+        return useTldDetect || useWebFragmentsDetect;
     }
 
     protected MetaInfConfiguration createMetaInfConfiguration() {
-        return new MetaInfConfiguration();
+        return new SelectableMetaInfConfiguration(useMetaInfoResourceDetect, useWebFragmentsDetect, useTldDetect, webFragmentsSelector);
+    }
+
+    public static class SelectableMetaInfConfiguration extends MetaInfConfiguration {
+
+        protected final boolean useMetaInfoResourceDetect;
+        protected final boolean useWebFragmentsDetect;
+        protected final boolean useTldDetect;
+        protected final Predicate<String> webFragmentsSelector;
+
+        public SelectableMetaInfConfiguration(boolean useMetaInfoResourceDetect, boolean useWebFragmentsDetect, boolean useTldDetect,
+                Predicate<String> webFragmentsSelector) {
+            this.useMetaInfoResourceDetect = useMetaInfoResourceDetect;
+            this.useWebFragmentsDetect = useWebFragmentsDetect;
+            this.useTldDetect = useTldDetect;
+            this.webFragmentsSelector = webFragmentsSelector;
+        }
+
+        @Override
+        public void scanForResources(WebAppContext context, Resource target, ConcurrentHashMap<Resource, Resource> cache) throws Exception {
+            if (useMetaInfoResourceDetect) {
+                if (isTargetWebFragment(target)) { // depends on web-fragment detect (to be same specification as tomcat)
+                    super.scanForResources(context, target, cache);
+                }
+            }
+        }
+
+        @Override
+        public void scanForFragment(WebAppContext context, Resource jar, ConcurrentHashMap<Resource, Resource> cache) throws Exception {
+            if (useWebFragmentsDetect && isTargetWebFragment(jar)) {
+                super.scanForFragment(context, jar, cache);
+            }
+        }
+
+        protected boolean isTargetWebFragment(Resource jar) {
+            return webFragmentsSelector != null && webFragmentsSelector.test(jar.getName());
+        }
+
+        @Override
+        public void scanForTlds(WebAppContext context, Resource jar, ConcurrentHashMap<Resource, Collection<URL>> cache) throws Exception {
+            if (useTldDetect) {
+                super.scanForTlds(context, jar, cache);
+            }
+        }
     }
 
     protected boolean isValidAnnotationConfiguration() {
@@ -394,6 +517,67 @@ public class JettyBoot {
 
     protected JettyWebXmlConfiguration createJettyWebXmlConfiguration() {
         return new JettyWebXmlConfiguration();
+    }
+
+    // -----------------------------------------------------
+    //                                         Classpath Jar
+    //                                         -------------
+    protected void setupClasspathJarResourceIfNeeds(WebAppContext context) {
+        if (isWarableWorld() || !isValidMetaInfConfiguration()) {
+            return;
+        }
+        // may be local development and uses meta-inf configuration here
+        final List<String> classpathList = extractJarClassspathList();
+        for (String classpath : classpathList) {
+            final String jarPath = convertClasspathToJarPath(classpath);
+            final URL url;
+            try {
+                url = new URL(jarPath);
+            } catch (MalformedURLException e) {
+                throw new IllegalStateException("Failed to create URL from the jar path: " + jarPath, e);
+            }
+            context.getMetaData().addContainerResource(new JarResource(url) {
+            });
+        }
+    }
+
+    protected boolean isWarableWorld() {
+        final URL warLocation = getWarLocation();
+        final String path;
+        try {
+            path = warLocation.toURI().getPath();
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Failed to get the war URI: " + warLocation, e);
+        }
+        return path != null && isWarableFile(path);
+    }
+
+    protected List<String> extractJarClassspathList() {
+        final String classpathExp = System.getProperty("java.class.path");
+        if (classpathExp == null) {
+            return Collections.emptyList();
+        }
+        return Stream.of(classpathExp.split(":")).filter(classpath -> {
+            if (!classpath.endsWith(".jar")) {
+                return false;
+            }
+            // not perfect allowed, remove them as possible
+            if (classpath.startsWith("/Library/Java/") || classpath.startsWith("/System/Library/Java")) { // for MacOSX
+                return false;
+            }
+            final int lastSlashIndex = classpath.lastIndexOf("/");
+            if (lastSlashIndex >= 0) { // basically here
+                final String pathOnly = classpath.substring(0, lastSlashIndex); // without file name
+                if (pathOnly.endsWith("/jre/lib") || pathOnly.endsWith("/jre/lib/ext")) { // for Windows (also MacOSX)
+                    return false;
+                }
+            }
+            return true;
+        }).collect(Collectors.toList());
+    }
+
+    protected String convertClasspathToJarPath(String classpath) {
+        return "jar:file:" + classpath + "!/";
     }
 
     // -----------------------------------------------------
